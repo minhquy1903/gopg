588e9e0b9881834c3ce1b299d26a8b6c4caae034
vominhquy <vominhquy1903@gmail.com>
2024-04-29 15:38:47 +0700

    simple go tmpl example

--- main.go
+++ main.go
@@ -1,5 +1,56 @@
 package main
 
-func main() {
+import (
+	"html/template"
+	"io"
+	"io/fs"
+	"net/http"
+	"path/filepath"
 
+	"github.com/labstack/echo/v4"
+)
+
+type Templates struct {
+	templates *template.Template
+}
+
+func (t *Templates) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func NewTemplate() *Templates {
+	tmpl := template.New("")
+
+	err := filepath.Walk("views", func(path string, info fs.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() && filepath.Ext(path) == ".html" {
+			tmpl.ParseFiles(path)
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	return &Templates{
+		templates: tmpl,
+	}
+}
+
+func main() {
+	e := echo.New()
+
+	e.Renderer = NewTemplate()
+
+	e.Static("/", "public")
+
+	e.GET("/", func(c echo.Context) error {
+		return c.Render(http.StatusOK, "index", map[string]interface{}{})
+	})
+
+	e.Start(":3000")
 }
